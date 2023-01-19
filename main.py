6ba3519a8ef5304214bbc39821c7b9f502e2a866
palliotte <114391946+palliotte@users.noreply.github.com>
2023-01-19 11:39:09 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 def on_button_pressed_a():
-    if padletA.get(LedSpriteProperty.X) > 0:
-        padletA.change(LedSpriteProperty.X, -1)
-        padletB.change(LedSpriteProperty.X, -1)
-        radio.send_value("x", padletA.get(LedSpriteProperty.X))
+    if ennemieA:
+        if padletA.get(LedSpriteProperty.X) > 0:
+            padletA.change(LedSpriteProperty.X, -1)
+            padletB.change(LedSpriteProperty.X, -1)
+            radio.send_value("x", padletA.get(LedSpriteProperty.X))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    if padletB.get(LedSpriteProperty.X) < 4:
-        padletA.change(LedSpriteProperty.X, 1)
-        padletB.change(LedSpriteProperty.X, 1)
-        radio.send_value("x", padletA.get(LedSpriteProperty.X))
+    if ennemieA:
+        if padletB.get(LedSpriteProperty.X) < 4:
+            padletA.change(LedSpriteProperty.X, 1)
+            padletB.change(LedSpriteProperty.X, 1)
+            radio.send_value("x", padletA.get(LedSpriteProperty.X))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_received_value(name, value):
@@ -27,6 +29,7 @@ radio.on_received_value(on_received_value)
 score = 0
 ennemieB: game.LedSprite = None
 ball: game.LedSprite = None
+host = 0
 ennemieA: game.LedSprite = None
 padletB: game.LedSprite = None
 padletA: game.LedSprite = None
@@ -36,38 +39,14 @@ padletB = game.create_sprite(3, 4)
 padletB.set(LedSpriteProperty.BRIGHTNESS, 50)
 game.set_life(10)
 if not (ennemieA):
-    ball = game.create_sprite(2, 2)
-    x = randint(-1, 1)
-    y = 1
-    while True:
-        game.pause()
-        if ennemieA:
-            game.resume()
-            break
+    host = 1
+    if host == 1:
+        ball = game.create_sprite(2, 2)
 
 def on_every_interval():
     global score
     radio.send_value("x", padletA.get(LedSpriteProperty.X))
-    score += 1
+    if ennemieA:
+        score += 1
 loops.every_interval(1000, on_every_interval)
 
-def on_forever():
-    ball.change(LedSpriteProperty.X, directionX)
-    ball.change(LedSpriteProperty.Y, directionY)
-    pause(200)
-    if ball.is_touching(padletA) or ball.is_touching(padletB):
-        ball.change(LedSpriteProperty.Y, -1)
-        directionY = -1
-        directionX = randint(-1, 1)
-    elif ball.get(LedSpriteProperty.Y) >= 4:
-        ball.set(LedSpriteProperty.BLINK, 0)
-        basic.pause(2000)
-        game.game_over()
-    if ball.get(LedSpriteProperty.X) <= 0:
-        directionX = 1
-    elif ball.get(LedSpriteProperty.X) >= 4:
-        directionX = -1
-    if ball.get(LedSpriteProperty.Y) <= 0:
-        directionY = 1
-    basic.pause(100)
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,22 @@
-let x: number;
-let y: number;
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    if (padletA.get(LedSpriteProperty.X) > 0) {
-        padletA.change(LedSpriteProperty.X, -1)
-        padletB.change(LedSpriteProperty.X, -1)
-        radio.sendValue("x", padletA.get(LedSpriteProperty.X))
+    if (ennemieA) {
+        if (padletA.get(LedSpriteProperty.X) > 0) {
+            padletA.change(LedSpriteProperty.X, -1)
+            padletB.change(LedSpriteProperty.X, -1)
+            radio.sendValue("x", padletA.get(LedSpriteProperty.X))
+        }
+        
     }
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    if (padletB.get(LedSpriteProperty.X) < 4) {
-        padletA.change(LedSpriteProperty.X, 1)
-        padletB.change(LedSpriteProperty.X, 1)
-        radio.sendValue("x", padletA.get(LedSpriteProperty.X))
+    if (ennemieA) {
+        if (padletB.get(LedSpriteProperty.X) < 4) {
+            padletA.change(LedSpriteProperty.X, 1)
+            padletB.change(LedSpriteProperty.X, 1)
+            radio.sendValue("x", padletA.get(LedSpriteProperty.X))
+        }
+        
     }
     
 })
@@ -34,6 +38,7 @@ radio.onReceivedValue(function on_received_value(name: string, value: number) {
 let score = 0
 let ennemieB : game.LedSprite = null
 let ball : game.LedSprite = null
+let host = 0
 let ennemieA : game.LedSprite = null
 let padletB : game.LedSprite = null
 let padletA : game.LedSprite = null
@@ -43,49 +48,18 @@ padletB = game.createSprite(3, 4)
 padletB.set(LedSpriteProperty.Brightness, 50)
 game.setLife(10)
 if (!ennemieA) {
-    ball = game.createSprite(2, 2)
-    x = randint(-1, 1)
-    y = 1
-    while (true) {
-        game.pause()
-        if (ennemieA) {
-            game.resume()
-            break
-        }
-        
+    host = 1
+    if (host == 1) {
+        ball = game.createSprite(2, 2)
     }
+    
 }
 
 loops.everyInterval(1000, function on_every_interval() {
     
     radio.sendValue("x", padletA.get(LedSpriteProperty.X))
-    score += 1
-})
-basic.forever(function on_forever() {
-    let directionY: number;
-    let directionX: number;
-    ball.change(LedSpriteProperty.X, directionX)
-    ball.change(LedSpriteProperty.Y, directionY)
-    pause(200)
-    if (ball.isTouching(padletA) || ball.isTouching(padletB)) {
-        ball.change(LedSpriteProperty.Y, -1)
-        directionY = -1
-        directionX = randint(-1, 1)
-    } else if (ball.get(LedSpriteProperty.Y) >= 4) {
-        ball.set(LedSpriteProperty.Blink, 0)
-        basic.pause(2000)
-        game.gameOver()
+    if (ennemieA) {
+        score += 1
     }
     
-    if (ball.get(LedSpriteProperty.X) <= 0) {
-        directionX = 1
-    } else if (ball.get(LedSpriteProperty.X) >= 4) {
-        directionX = -1
-    }
-    
-    if (ball.get(LedSpriteProperty.Y) <= 0) {
-        directionY = 1
-    }
-    
-    basic.pause(100)
 })
